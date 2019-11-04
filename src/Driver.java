08a30cd842367874dd056320307877edf5a24c8f
YukiSugaya <sugaya324@gmail.com>
2019-11-03 21:52:49 -0800

    Add task2

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Driver {
-    
+    public static void main(String[] args) {
+        while(true) {
+            Scanner in = new Scanner(System.in);
+            String prompt = in.nextLine();
+            String usernameInput = InputCollector.getUserInput(prompt);
+            if (usernameInput.equals("quit")) {
+                System.exit(0);
+            } else {
+                System.out.println(usernameInput);
+            }
+        }
+    }
 }
